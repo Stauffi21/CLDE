da6ea5b6290f28d87a78c8d4dbe1e4f269a53fcf
Stauffi21 <43137301+Stauffi21@users.noreply.github.com>
2024-12-13 15:58:50 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,1 @@
-// server.js
-const express = require('express');
-const path = require('path');
 
-const app = express();
-const port = 3000;
-
-// Statische Dateien aus dem dist-Ordner bereitstellen
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Alle Routen zu index.html weiterleiten (für Angular Routing)
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Server läuft unter http://localhost:${port}`);
-});
